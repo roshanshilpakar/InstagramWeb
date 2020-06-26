90bf975af00cd5092fdf4b89228d6a022f225f3a
roshanshilpakar <36304491+roshanshilpakar@users.noreply.github.com>
2020-06-26 08:36:30 +0530

    ok

--- server/app.js
+++ server/app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+
+const customMiddleware = (req, res, next) => {
+    console.log('middleware executed')
+    next()
+}
+
+app.use(customMiddleware)
+
 app.get('/', (req, res) => {
+    console.log('home')
     res.send('hello roshan')
 })
 
